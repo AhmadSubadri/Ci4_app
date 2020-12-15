3ffe39d22fea9d2945fd8b864add05888f0f5607
Ahmad Subadri <ahmadsubadri953@gmail.com>
2020-12-15 19:40:27 +0700

    Controller model buku

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index()
+	{
+        echo view('layout/header');
+        echo view('pages/home');
+        echo view('layout/footer');
+    }
+    
+    public function about(){
+        echo view('layout/header');
+        echo view('pages/about');
+        echo view('layout/footer');
+    }
+	//--------------------------------------------------------------------
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,1 @@
+<h1>Ini halaman About</h1>
