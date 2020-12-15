962e0d070328cca2975d3c7d6fefdae095d0d4a8
Ahmad Subadri <ahmadsubadri953@gmail.com>
2020-12-15 19:58:59 +0700

    contol model tabel data

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -1,1 +1,12 @@
 <?php namespace App\Controllers;
+
+class Buku extends BaseController{
+    public function index(){
+        $data = [
+            'title' => 'Daftar Buku'
+        ];
+        echo view('layout/header',$data);
+        echo view('buku/index');
+        echo view('layout/footer');
+    }
+}
---
+++ app/Views/buku/index.php
@@ -0,0 +1,18 @@
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Sampul</th>
+            <th scope="col">Judul Buku</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td><img src="" alt=""></td>
+            <td>Proyek Perangkat Lunak</td>
+            <td><a href="" class="btn btn-success">Detail</a></td>
+        </tr>
+    </tbody>
+</table>
